5a986a6c49cb257de3fdbe0719acc81fd5947c1a
Burak Yazan <brkyznn@gmail.com>
2022-09-13 00:04:19 +0300

    add functionality to back button

--- src/components/navigation/BackButton.js
+++ src/components/navigation/BackButton.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components/native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'; 
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native';
 
-function BackButton() {
-  return <Icon name="arrow-back-ios" size={24} color="black"/>
+export default function BackButton() {
+  const navigation = useNavigation();
+
+  return(
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="arrow-back-ios" size={24} color="black"/>
+    </TouchableOpacity>
+  )
 }
 
 const Icon = styled(MaterialIcons)`
   margin-left: 20px
 `;
-
-export default BackButton;
