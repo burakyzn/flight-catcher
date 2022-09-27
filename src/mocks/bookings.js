1e2dd5c969f3b08c04a03441319d9d1c8f5f24c3
Burak Yazan <brkyznn@gmail.com>
2022-09-27 20:57:27 +0300

    create Booking screen with mock data

---
+++ src/mocks/bookings.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    code: "TK3201",
+    from: "IST (Istanbul)",
+    to: "BER (Berlin)",
+    fromAirport: "Istanbul Sabiha Gokcen International Airport",
+    toAirport: "Berlin Brandenburg Airport",
+    departureTime: "7:55",
+    arrivalTime: "09:45",
+    date: "15/10/2022",
+    cabinClass: "Business Class",
+    flightTime: "2hr 50min",
+    gate: 10
+  },
+  {
+    id: 2,
+    code: "PC1135",
+    from: "SAW (Istanbul)",
+    to: "BER (Berlin)",
+    fromAirport: "Istanbul Sabiha Gokcen International Airport",
+    toAirport: "Berlin Brandenburg Airport",
+    departureTime: "10:10",
+    arrivalTime: "12:10",
+    date: "15/10/2022",
+    cabinClass: "Business Class",
+    flightTime: "3hr 0min",
+    gate: 11
+  },
+  {
+    id: 3,
+    code: "TK3434",
+    from: "IST (Istanbul)",
+    to: "BER (Berlin)",
+    fromAirport: "Istanbul Sabiha Gokcen International Airport",
+    toAirport: "Berlin Brandenburg Airport",
+    departureTime: "12:15",
+    arrivalTime: "14:05",
+    date: "15/10/2022",
+    cabinClass: "Business Class",
+    flightTime: "2hr 50min",
+    gate: 12
+  },
+]
--- src/screens/Booking.js
+++ src/screens/Booking.js
@@ -1,17 +1,51 @@
 import styled from 'styled-components/native';
-import { Text } from "react-native";
+import BoardingDetail from '../components/boardingPass/BoardingDetail';
+import bookings from '../mocks/bookings';
+import Button from '../components/Button';
+import { FlatList } from 'react-native';
 
 export default function Booking() {
+  const renderItem = ({ item }) =>  (
+    <Wrapper>
+      <BoardingDetail flight={item} />
+      <ButtonArea>
+        <Button 
+          text="Modify"
+        />
+      </ButtonArea>
+    </Wrapper>
+  );
+  
   return (
     <View>
-      <Text>Booking Screen</Text>
+      <FlatList
+        style={{width: "100%"}}
+        data={bookings}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   )
 }
 
 const View = styled.View`
   flex: 1;
-  background-color: #fff;
-  align-items : center;
+  background-color: #F9FBFA;
   justify-content: center;
 `;
+
+const Wrapper = styled.View`
+  height: 420px;
+  border-radius: 16px;
+  background-color: #FFFFFF;
+  box-shadow: 0px 5px 10px rgba(89, 27, 27, 0.05);
+  padding: 16px 16px 0px 16px;
+  margin: 16px;
+`;
+
+const ButtonArea = styled.View`
+  flex:1;
+  width: 100%;
+  margin-top: 10px;
+`;
+
